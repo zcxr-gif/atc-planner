4054ab7cd15c502b1cc206c918a671d1d47e486e
zcxr-gif <hrantony2230@gmail.com>
2025-07-14 00:06:03 -0400

    Update 2

--- public/app.js
+++ public/app.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // --- MODIFIED: Use L.maplibreGL for vector tile styles ---
-    const mapTilerKey = "InN0UHhrnYhq86Wukvt6";
+    const mapTilerKey = "ety8GjHG3ccnoSZfOULB";
     const mapTilerAttribution = '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
 
     const lightModeMap = L.maplibreGL({
