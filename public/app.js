0e38b51bc18ee8201e6f5644914ed670ba02d1e5
zcxr-gif <hrantony2230@gmail.com>
2025-08-05 23:14:01 -0400

    Terrain 2

--- public/app.js
+++ public/app.js
@@ -361,11 +361,11 @@ function createVorCompassImage(size = 256) {
         map.on('load', () => {
             // *** MODIFICATION 2: Define the AWS terrain source and add a hillshade layer for visibility ***
             map.addSource('aws-terrain', {
-                type: 'raster-dem',
-                url: ['https://s3.amazonaws.com/elevation-tiles-prod/terrarium/{z}/{x}/{y}.png'],
-                encoding: 'terrarium', // This is crucial for AWS tiles
-                tileSize: 256
-            });
+    type: 'raster-dem',
+    url: 'https://s3.amazonaws.com/elevation-tiles-prod/terrarium/{z}/{x}/{y}.png',
+    tileSize: 256,
+    encoding: 'terrarium'
+});
 
             map.addLayer({
                 id: 'hillshade',
